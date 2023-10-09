619093aa5a3ab09cf2131491da90b4195384a2fb
leiver valenciano <leiver.valenciano@gmail.com>
2023-10-09 14:22:27 -0600

    Api y Hash del password

---
+++ ProjectAPI/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+
+        
+        
+    }
+}
