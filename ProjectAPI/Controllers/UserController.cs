564f410b9c37e41216be3be078f1eac7c50fcb5f
leiver valenciano <leiver.valenciano@gmail.com>
2023-10-24 20:58:44 -0600

    token rutas

--- ProjectAPI/Controllers/UserController.cs
+++ ProjectAPI/Controllers/UserController.cs
@@ -4,6 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using ProjectAPI.Context;
 using ProjectAPI.Helper;
 using ProjectAPI.Models;
+using System.Text;
+using System.Text.RegularExpressions;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProjectAPI.Controllers
 {
@@ -23,32 +30,125 @@ namespace ProjectAPI.Controllers
             if (userObj == null)
                 return BadRequest();
 
-            var user = await _authContext.Users.
-                FirstOrDefaultAsync(x=> x.Username == userObj.Username && x.Password == userObj.Password);
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == userObj.Username);
+
             if (user == null)
-                return NotFound(new {Message = "User Not Found!"});
+                return NotFound(new { Message = "¡Usuario no encontrado!" });
+
+            if (!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new { Message = "La contraseña es incorrecta" });
+            }
+
+            user.Token = CreateJwt(user);
+
 
             return Ok(new
             {
-                Message = "Login Success"
-            });
+                Token = user.Token,
+                Message = "Acceso exitoso"
+            }) ;
         }
+
 
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] User userObj)
         {
             if (userObj == null)
                 return BadRequest();
+
+            //verificar usuario
+            if(await CheckUserNameExist(userObj.Username))
+            {
+                return BadRequest(new { Message = "¡El nombre de usuario ya existe!" });
+            }
+
+            if (!IsValidEmail(userObj.Email))
+            {
+                return BadRequest(new { Message = "El formato del correo electrónico no es válido." });
+            }
+
+            //verificar email
+            if (await CheckEmailExist(userObj.Email))
+            {
+                return BadRequest(new { Message = "¡El correo ya existe!" });
+            }
+
+
+            //comprobar la seguridad de la contraseña 
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString() });
+
+
             userObj.Password = PasswordHasher.HashPassword(userObj.Password);
-            userObj.Role = "User";
+            userObj.Role = "Admin";
             userObj.Token = "";
 
             await _authContext.Users.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
             return Ok(new
             {
-                Message = "User Registered"
+                Message = "Usuario registrado"
             });
+        }
+
+        private Task<bool> CheckUserNameExist(string username)
+            => _authContext.Users.AnyAsync(x => x.Username == username);
+
+
+        private bool IsValidEmail(string email)
+        {
+            // Utiliza una expresión regular para validar el formato del correo electrónico.
+            // Puedes ajustar la expresión regular según tus requisitos específicos.
+            string emailPattern = @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+        private Task<bool> CheckEmailExist(string email)
+            => _authContext.Users.AnyAsync(x => x.Email == email);
+
+
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8)
+                sb.Append("La longitud mínima de la contraseña debe ser 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]")
+                && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("La contraseña debe ser alfanumérica" + Environment.NewLine);
+            if (!Regex.IsMatch(password, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+                sb.Append("La contraseña debe contener un carácter especial." + Environment.NewLine);
+            return sb.ToString();
+        }
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("muymuysecretosecretomuymuysecreto");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}")
+
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.UtcNow.AddSeconds(5),
+                SigningCredentials = credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>>GetAllUsers()
+        {
+            return Ok(await _authContext.Users.ToListAsync());
         }
 
     }
--- ProjectAPI/Program.cs
+++ ProjectAPI/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjectAPI.Context;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,24 @@ builder.Services.AddCors(option =>
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
     DbContextOptionsBuilder dbContextOptionsBuilder = option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnStr"));
+});
+
+builder.Services.AddAuthentication(x =>{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("muymuysecretosecretomuymuysecreto")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
 });
 
 var app = builder.Build();
